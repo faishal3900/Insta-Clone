c7226a02008414796df44edf4416047b7dc3edb1
faishal3900 <faishal3900@gmail.com>
2025-07-16 09:12:38 +0530

    update backend and add frontend

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -1,18 +1,29 @@
 const express=require("express")
-const user = require("../models/auth")
+const User = require("../models/auth")
+const Post= require("../models/post")
 const LoginReq = require("../middleware/loginReq")
 const router=express.Router()
 
 
 router.get('/alluser', LoginReq,(req,res)=>{
-  user.find().then((user)=>{
+  User.find().then((user)=>{
     return res.status(200).json({user});
     
   });
 });
 
 
-router.get('/profile/:id', LoginReq,()=>{
-
+router.get('/profile/:id',(req,res)=>{
+User.findOne({_id: req.params.id})
+    .then((data)=>{
+      console.log(data)
+      Post.find({postedBY: req.params.id})
+          .then((data)=> {res.json(data)
+            console.log(data);
+            
+          })
+          .catch(err=> console.log(err)
+          )
+    })
 })
 module.exports = router
